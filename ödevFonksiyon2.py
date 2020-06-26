a7825cae1fc7e95a47d0d8f42d03ee7df5a7f3c7
GillsEffect <mustafag0000@gmail.com>
2020-06-26 21:10:32 +0300

    lise3 hw update

---
+++ ödevFonksiyon2.py
@@ -0,0 +1,8 @@
+#Aşağıdaki tanımlanmış özelliklere sahip bir fonksiyon yazın.
+
+# 1- fonksiyonun adı makeList olacak.
+# 2- number adından bir parametre alacak
+# 3- 0'dan (dahil) aldığı number adındaki parametreye kadar doğal sayıları içeren
+#     bir liste döndürecek.
+#     örneğin makeList(12) --> [0,1,2,3,4,5,6,7,8,9,10,11,12] listesini döndürecek.
+# 4- number parametresinin default değeri olmayacak.
---
+++ ödevFonksiyon3.py
@@ -0,0 +1,7 @@
+#Aşağıdaki tanımlanmış özelliklere sahip bir fonksiyon yazın.
+
+# 1- fonksiyonun adı makeFibonacci olacak.
+# 2- number adında bir parametre alacak.
+# 3- fibonacci serisinin, number'dan küçük ilk elemanına kadar olan elamanlarını
+#    içeren bir liste döndürecek.
+#    örneğin: makeFibonacci(15) ----> [1,1,2,3,5,8,13] listesini döndürecek.
