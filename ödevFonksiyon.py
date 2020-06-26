923fc24fb2e50c511348d81c36259dfcb0c2e41f
GillsEffect <mustafag0000@gmail.com>
2020-06-26 12:41:46 +0300

    lise3 ödev fonksiyon

---
+++ ödevFonksiyon.py
@@ -0,0 +1,10 @@
+#Aşağıdaki tanımlanmış özelliklere sahip bir fonksiyon yazın.
+
+# 1- fonksiyonun adı deleteDuplicated olacak.
+# 2- liste adında tek bir parametre alacak.
+# 3- verilen liste parametresinin içinde 1'den fazla kez bulunan ögeleri silecek.
+#       örneğin [1,2,2,2,2,3,3,3,4,5,6,6,6] --> [1,2,3,4,5,6]
+#               [1,3,3,4,7,8,9,9]           --> [1,3,4,7,8,9]
+# 4- fonksiyon parametresiz çalıştırıldığında default'u boş liste ([]) ile çalışacak
+# 5- liste parametresi eğer list data tipinde değilse boş liste ([]) dönecek, değilse
+#    sadeleştirilmiş listeyi dönecek.
